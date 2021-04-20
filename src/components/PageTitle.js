6b5ac44ffff5074b1fed46cfe9fc48d8fbc393f1
DMOrrock <50670252+DMOrrock@users.noreply.github.com>
2021-04-20 17:59:27 -0600

    Page title bug fix

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,5 +1,5 @@
 export default function PageTitle(props) {
-	if (props.PageTitleVisibility) {
+	if (props.visibility) {
 		return (
 			<div className="bg-white shadow">
 				<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
