c8258ecebc230ba5c318ba252be15658e35129ec
DMOrrock <50670252+DMOrrock@users.noreply.github.com>
2021-04-21 09:57:02 -0600

    both api and frontend published

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// export default axios.create({
+// 	baseURL:"http://localhost:3006/"
+// });
+
 export default axios.create({
-	baseURL:"http://localhost:3006/"
+	baseURL:"https://my-json-server.typicode.com/DMOrrock/built-app-backend/"
 });
